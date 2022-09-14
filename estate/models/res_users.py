fe8b681d1254f33d298e1ec0b415fd8bc54fca76
pporgis <pavel.prucha@orgis.cz>
2022-09-14 11:26:15 +0200

    Add project Custom

---
+++ estate/models/res_users.py
@@ -0,0 +1,7 @@
+from odoo import fields, models, api
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    property_ids = fields.One2many("estate.property", "user_id", domain="['|', ('state', '=', 'offer received'), ('state', '=', 'new')]")
+    nam = fields.Char()
